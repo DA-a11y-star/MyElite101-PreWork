34970ba5e90d4e47c2e6a27acb31fdba7ae2aeae
DA-a11y-star <87615327+DA-a11y-star@users.noreply.github.com>
2023-09-26 09:56:47 +0000

    I have fully set up my base Chatbot
    I have completed all the steps outlined in my Jira.
    1. Initialize Chatbot Project: Set up the basic code structure and environment for the chatbot project.
    2. Create Welcome Message Functionality: Implement the feature to welcome the user at the beginning of the
    conversation.
    3. Implement User Information Collection: Code the part of the chatbot that collects the user's name and age.
    4. Design Conversation Menu: Design the structure for the conversation menu, including placeholder options.
    5. Develop User Assistance Query: Write the code for asking the user how the chatbot can assist them.
    6. Implement Placeholder Options: Code the placeholder options for the conversation, including their
    corresponding responses.
    7. Add Exit Option in Menu: Implement the option for the user to exit the conversation with an appropriate
    goodbye message.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+def main():
+  print("Welcom to the Elite 101 Chatbot!")
+  name = input("Please enter your name: ")
+  age = int(input(f'Nice to meet you, {name}! How old are you? '))
+  if age <= 27:
+    print(f'Welcome {name}! Oh, to be {age} again! How can I help you today?')
+  elif age >= 27 and age <=50:
+    print(f'Welcome {name}! {age} some of the best years of life! How can I help you?')
+  else: 
+    print(f'Welcome {name}! {age} a very wise age! How can I help you?')
+  print("""
+1. Get Your Drivers Discence Issued
+2. Get Your Permit
+3. Talk to Customer Service
+4. Exit the Conversation""")
+  response1 = int(input("Enter the number of your choice: "))
+  if response1 == 1:
+    test = input("Have you taken your test yet: ")
+  elif response1 == 1:
+    course = input("Have you bought the Drivers Ed course: ")
+  elif response1 == 1:
+    print("Unfortunately Customer Service is unavailable today.")
+  print(f"Have a wonderful rest of your day and come back again sometime!")
+
+if __name__ == "__main__":
+    main()
